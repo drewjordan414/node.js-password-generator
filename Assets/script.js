f187642b26d2e4a469c5f7bc511c299050507724
Drew Jordan <ajordan12@luc.edu>
2023-05-17 17:01:58 -0500

    update

--- Assets/script.js
+++ Assets/script.js
@@ -7,14 +7,10 @@ var instruct = alert("Please click the generate password button to start the pro
 function writePassword() {
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 function writePassword() {
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
@@ -22,29 +18,20 @@ function writePassword() {
   passwordText.value = password;
   console.log(passwordText);
 }
-
 // listen to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
+// generate password function
 function generatePassword() {
   // add an empty array to store the password
   var password = ''; // empty string to store the password
-
   // line 36,,,, you might be giving me some trouble
   var passwordLength = prompt("Enter a length between 8 and 128 characters long"); 
   // loop to check if the password is between 8 and 128 chars
-
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("Please enter a valid number between 8 and 128");
     console.log(passwordLength);
     return;
   }
-  // var spc = prompt("Do you want to include special characters?");
-  // var num = prompt("Do you want to include numbers?");
-  // var lowercase = prompt("Do you want to include lowercase letters?");
-  // var uppercase = prompt("Do you want to include uppercase letters?");
-  // confirm to include uppercase letters, lowercase letters, numeric, special characters
   apc = ''; // all possible characters
   if (confirm("Do you want to include special characters?")) {
     apc += '!@#$%^&*()_+';
