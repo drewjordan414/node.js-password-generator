1c0f84a73ce34cbe3fc89f8b87baf32c029271d0
Drew Jordan <ajordan12@luc.edu>
2023-05-17 23:57:29 -0500

    update

--- Assets/script.js
+++ Assets/script.js
@@ -55,9 +55,6 @@ function generatePassword() {
   }
   return password;
 }
-
-
-
   // spc logic
 //   if (spc, num, lowercase, uppercase === "yes", "Yes", "YES") {
 //     apc = apc.concat(spc, num, lowercase, uppercase);
@@ -116,31 +113,3 @@ function generatePassword() {
 //     }
 // }
 
-
-// ask for length of the password
-  // prompt
-
-// check the length provided by the user is a number in between 8 and 128
-
-
-// confirm : 4 for uppercase, lowercase, numeric, special characters
-
-
-  // save it to a variable
-
-
-// check if one of the uppercase or lowercase or numeric or special characters is there
-
-
-// combine the confirmed arrays
-
-
-// create an array for uppercase letters, lowercase letters, numeric, special characters
-
-
-// create an array variable to have a new password with length provided by the user []
-
-
-// random item form combined array and push it to new password array and do this step up to the length of user input
-
-// return the new password
